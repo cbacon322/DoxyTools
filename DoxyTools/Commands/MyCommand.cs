19d50e334d61749676fe572bb5d86b6b569a873f
AATRIX\ChaseB <chaseb@aatrix.com>
2023-12-22 15:08:04 -0600

    error-handling, comments, clean-up

--- DoxyTools/Commands/MyCommand.cs
+++ DoxyTools/Commands/MyCommand.cs
@@ -1,84 +1,109 @@
-﻿// MyCommand.cs
-
-using System;
+﻿using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
 using EnvDTE;
 using EnvDTE80;
 using Microsoft.VisualStudio;
-using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Interop;
-using System.Threading.Tasks;
-using System.Runtime.InteropServices;
-using Microsoft.VisualStudio.Threading;
-using Microsoft;
+using OutputWindowPane = EnvDTE.OutputWindowPane;
+using Process = System.Diagnostics.Process;
+using Project = EnvDTE.Project;
 
-namespace DoxyTools
+namespace DoxyTools.Commands
 {
+    // Command implementation for generating Doxygen documentation.
     [Command(PackageIds.GenerateDocs)]
     internal sealed class MyCommand : BaseCommand<MyCommand>
     {
+        // Executes when the command is invoked.
         protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
         {
+            // Ensure execution on the main thread.
             await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
-
-            // Get the IVsMonitorSelection service to find the current hierarchy
-            var monitorSelection = ServiceProvider.GlobalProvider.GetService(typeof(SVsShellMonitorSelection)) as IVsMonitorSelection;
-            var hierarchyPtr = IntPtr.Zero;
-            var project = GetSelectedProject(monitorSelection, out hierarchyPtr);
-
-            if (project == null)
-            {
-                // Handle error - no project selected
-                return;
-            }
-
-            var projectPath = Path.GetDirectoryName(project.FullName);
-            if (projectPath == null)
-            {
-                // Handle error - unable to get project path
-                return;
-            }
-
-            var doxyfilePath = Path.Combine(projectPath, "Doxyfile");
-            if (!File.Exists(doxyfilePath))
-            {
-                // Handle error - Doxyfile not found
-                return;
-            }
-
-            RunDoxygen(doxyfilePath);
-        }
-
-        private EnvDTE.Project GetSelectedProject(IVsMonitorSelection monitorSelection, out IntPtr hierarchyPtr)
-        {
-            hierarchyPtr = IntPtr.Zero;
-            IntPtr hierarchy = IntPtr.Zero;
-            uint itemId = (uint)VSConstants.VSITEMID.Nil;
-            IVsMultiItemSelect multiItemSelect = null;
-            IntPtr selectionContainerPtr = IntPtr.Zero;
 
             try
             {
-                monitorSelection.GetCurrentSelection(out hierarchy, out itemId, out multiItemSelect, out selectionContainerPtr);
+                // Get the current selection from the Visual Studio environment.
+                IVsMonitorSelection monitorSelection = ServiceProvider.GlobalProvider.GetService(typeof(SVsShellMonitorSelection)) as IVsMonitorSelection;
+                Project project = GetSelectedProject(monitorSelection, out _);
 
+                // Check if a project is selected.
+                if (project == null)
+                {
+                    // Show error message in Output Window if no project is selected.
+                    await ShowMessageInOutputWindowAsync("No project selected.");
+                    return;
+                }
+
+                // Get the project directory.
+                string projectDirectory = Path.GetDirectoryName(project.FullName);
+                if (projectDirectory == null)
+                {
+                    // Throw an exception if the project directory cannot be determined.
+                    throw new InvalidOperationException("Project directory cannot be determined.");
+                }
+
+                // Construct the path to the Doxyfile.
+                string doxyfilePath = Path.Combine(projectDirectory, "Doxyfile");
+
+                // Check if Doxyfile exists in the project directory.
+                if (!File.Exists(doxyfilePath))
+                {
+                    // Show error message in Output Window if Doxyfile is not found.
+                    await ShowMessageInOutputWindowAsync("Doxyfile not found.");
+                    return;
+                }
+
+                // Run Doxygen with the path to the Doxyfile.
+                await RunDoxygen(doxyfilePath);
+            }
+            catch (Exception ex)
+            {
+                // Show general error message in Output Window.
+                await ShowMessageInOutputWindowAsync($"Error occurred: {ex.Message}");
+            }
+        }
+
+        // Retrieves the selected project from the current Visual Studio environment.
+        private static EnvDTE.Project GetSelectedProject(IVsMonitorSelection monitorSelection, out IntPtr hierarchyPtr)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            hierarchyPtr = IntPtr.Zero;
+            IntPtr hierarchy = IntPtr.Zero;
+            IVsMultiItemSelect multiItemSelect = null;
+            IntPtr selectionContainerPtr = IntPtr.Zero;
+            
+            try
+            {
+                // Get the current selection in the Solution Explorer.
+                monitorSelection.GetCurrentSelection(out hierarchy, out uint itemId, out multiItemSelect, out selectionContainerPtr);
+
+                // Check if a project is selected.
                 if (hierarchy != IntPtr.Zero && itemId == (uint)VSConstants.VSITEMID.Root)
                 {
                     IVsHierarchy selectedHierarchy = Marshal.GetTypedObjectForIUnknown(hierarchy, typeof(IVsHierarchy)) as IVsHierarchy;
                     if (selectedHierarchy is IVsProject vsProject)
                     {
-                        object projectObject;
-                        ErrorHandler.ThrowOnFailure(selectedHierarchy.GetProperty(itemId, (int)__VSHPROPID.VSHPROPID_ExtObject, out projectObject));
+                        // Retrieve the project object.
+                        ErrorHandler.ThrowOnFailure(selectedHierarchy.GetProperty(itemId, (int)__VSHPROPID.VSHPROPID_ExtObject, out object projectObject));
 
-                        EnvDTE.Project project = projectObject as EnvDTE.Project;
-                        return project;
+                        // Return the selected project.
+                        return projectObject as EnvDTE.Project;
                     }
                 }
 
-                return null;
+                return null; // Return null if no project is found.
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Error in GetSelectedProject: {ex.Message}");
+                return null; // Return null in case of an exception.
             }
             finally
             {
+                // Release allocated resources.
                 if (hierarchy != IntPtr.Zero)
                 {
                     Marshal.Release(hierarchy);
@@ -94,44 +119,88 @@ namespace DoxyTools
             }
         }
 
-        private async Task<EnvDTE.OutputWindowPane> GetOutputWindowPaneAsync(string paneName, bool create = true)
+        // Gets an Output Window Pane by name or creates it if it doesn't exist.
+        private static async Task<EnvDTE.OutputWindowPane> GetOutputWindowPaneAsync(string paneName, bool create = true)
         {
+            // Ensure execution on the main thread.
             await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
 
+            try
+            {
+                if (await ServiceProvider.GetGlobalServiceAsync(typeof(SDTE)) is not DTE2 dte2)
+                {
+                    Debug.WriteLine("Unable to get DTE2 service.");
+                    return null;
+                }
+
+                var outputWindow = dte2.ToolWindows.OutputWindow;
+                EnvDTE.OutputWindowPane pane = null;
+
+                try
+                {
+                    // Attempt to retrieve the pane by name.
+                    pane = outputWindow.OutputWindowPanes.Item(paneName);
+                }
+                catch (ArgumentException)
+                {
+                    // Create the pane if it doesn't exist and 'create' is true.
+                    if (create)
+                    {
+                        pane = outputWindow.OutputWindowPanes.Add(paneName);
+                    }
+                }
+
+                return pane;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Error in GetOutputWindowPaneAsync: {ex.Message}");
+                return null;
+            }
+        }
+
+        // Shows a message in the Output Window.
+        private async Task ShowMessageInOutputWindowAsync(string message)
+        {
+            // Ensure execution on the main thread.
             DTE2 dte2 = await ServiceProvider.GetGlobalServiceAsync(typeof(SDTE)) as DTE2;
             if (dte2 == null)
             {
                 Debug.WriteLine("Unable to get DTE2 service.");
-                return null;
+                return;
             }
 
-            OutputWindow outputWindow = dte2.ToolWindows.OutputWindow;
-            EnvDTE.OutputWindowPane pane = null;
-
-            try
+            OutputWindowPane outputPane = await GetOutputWindowPaneAsync("Doxygen Output", true);
+            if (outputPane != null)
             {
-                pane = outputWindow.OutputWindowPanes.Item(paneName);
+                // Display the message in the Output Window.
+                outputPane.OutputString($"ERROR: {message}\n");
+                // Show the Output Window.
+                dte2.ExecuteCommand("View.Output", string.Empty);
             }
-            catch (ArgumentException)
-            {
-                if (create)
-                {
-                    pane = outputWindow.OutputWindowPanes.Add(paneName);
-                }
-            }
-
-            return pane;
         }
 
+        // Runs the Doxygen process with the specified Doxyfile.
         private async Task RunDoxygen(string doxyfilePath)
         {
+            // Ensure execution on the main thread.
             await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+
+            int errorCount = 0; // Initialize error count
 
             try
             {
+                DTE2 dte2 = await ServiceProvider.GetGlobalServiceAsync(typeof(SDTE)) as DTE2;
+                if (dte2 == null)
+                {
+                    Debug.WriteLine("Unable to get DTE2 service.");
+                    return;
+                }
+
                 Debug.WriteLine($"Running Doxygen for: {doxyfilePath}");
 
-                var startInfo = new ProcessStartInfo
+                // Configure the process start information.
+                ProcessStartInfo startInfo = new ProcessStartInfo
                 {
                     FileName = "doxygen",
                     Arguments = $"\"{doxyfilePath}\"",
@@ -139,39 +208,52 @@ namespace DoxyTools
                     RedirectStandardOutput = true,
                     RedirectStandardError = true,
                     CreateNoWindow = true,
-                    WorkingDirectory = Path.GetDirectoryName(doxyfilePath) // Set the working directory
+                    WorkingDirectory = Path.GetDirectoryName(doxyfilePath)
                 };
 
-                using (var process = System.Diagnostics.Process.Start(startInfo))
+                using (Process process = System.Diagnostics.Process.Start(startInfo))
                 {
-                    // Get the Output Window Pane
-                    var outputPane = await GetOutputWindowPaneAsync("Doxygen Output", true);
+                    // Get the Output Window Pane.
+                    OutputWindowPane outputPane = await GetOutputWindowPaneAsync("Doxygen Output", true);
                     if (outputPane == null)
                     {
                         Debug.WriteLine("Unable to get Output Window Pane.");
                         return;
                     }
 
-                    // Read standard output and write to the Output Window
+                    // Show the Output Window.
+                    dte2.ExecuteCommand("View.Output", string.Empty);
+
+                    // Read and display standard output and error in the Output Window.
                     while (!process.StandardOutput.EndOfStream)
                     {
                         string line = await process.StandardOutput.ReadLineAsync();
                         outputPane.OutputString(line + Environment.NewLine);
                     }
 
-                    // Read standard error and write to the Output Window
                     while (!process.StandardError.EndOfStream)
                     {
                         string line = await process.StandardError.ReadLineAsync();
                         outputPane.OutputString("ERROR: " + line + Environment.NewLine);
+                        errorCount++; // Increment error count
                     }
 
-                    process.WaitForExit();
+                    process.WaitForExit(); // Wait for the process to exit.
                 }
             }
             catch (Exception ex)
             {
-                Debug.WriteLine($"Error running Doxygen: {ex.Message}");
+                await ShowMessageInOutputWindowAsync($"Error running Doxygen: {ex.Message}");
+                errorCount++; // Increment error count
+            }
+            finally
+            {
+                // Output summary of errors.
+                OutputWindowPane outputPane = await GetOutputWindowPaneAsync("Doxygen Output", true);
+                if (outputPane != null)
+                {
+                    outputPane.OutputString($"Doxygen Generation Finished with {errorCount} Errors.\n");
+                }
             }
         }
     }
